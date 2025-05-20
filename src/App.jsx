836d91c49118ccab935179af0d1f2fc3628c4710
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-05-20 19:16:29 +0330

    add QueryClient to app

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,30 @@ import "./App.css";
 import "./index.css";
 import SelectTabel from "./components/Tabs/SelectTabel";
 import MapView from "./components/Map/Mapview";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className=" font-dana max-w-[627px] w-full mx-auto">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/map"
-            element={
-              <MapView
-                mapPoint={{ lat: 35.701825570554576, lng: 51.39133193872797 }}
-              />
-            }
-          />
-          <Route path="/product" element={<Product />} />
-          <Route path="/selectTabel" element={<SelectTabel />} />
-        </Routes>
-      </Router>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/map"
+              element={
+                <MapView
+                  mapPoint={{ lat: 35.701825570554576, lng: 51.39133193872797 }}
+                />
+              }
+            />
+            <Route path="/product" element={<Product />} />
+            <Route path="/selectTabel" element={<SelectTabel />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
